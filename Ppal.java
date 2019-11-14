2bb373652f841f112dbc64357353e045f0e3ed99
Alejandro Ramirez <alexrramos270@gmail.com>
2019-11-14 12:17:20 +0100

    Anadido el menú donde elegir las 4 operaciones y la opción de poner 0 para salir.

---
+++ Ppal.java
@@ -0,0 +1,46 @@
+package calculadora;
+
+import utilidades.Leer;
+
+public class Ppal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int op=1;
+		double num1, num2, resul;
+		
+		do {
+			
+			System.out.println("Bienvenido a la calculadora.\n"
+					+ "Pulsa 1 para sumar.\n"
+					+ "Pulsa 2 para restar.\n"
+					+ "Pulsa 3 para multiplicar.\n"
+					+ "Pulsa 4 para divivir.\n");
+			
+			op=Leer.datoInt();
+			
+			switch(op) {
+			
+			case 1:
+				
+				System.out.println("Vas a sumar.\n"
+						+ "Pon el primer número:");
+				
+				num1=Leer.datoDouble();
+				
+				System.out.println("Pon el segundo número:");
+				
+				num2=Leer.datoDouble();
+				
+				resul=num1+num2;
+				
+				System.out.println("La suma de " + num1 + "+" + num2 + " es " + resul);
+				
+			}
+			
+		}while (op!=0);
+		
+	}
+
+}
